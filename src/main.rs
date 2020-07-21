19b16fa39caffd2434fd5868b0a92d1120d2739a
Nico Audy <nicoaudy1994@gmail.com>
2020-07-21 09:37:37 +0700

    display todo list 🤙🏻

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use std::env;
 
+struct TodoItem {
+    name: String,
+    completed: char
+}
+
 fn main() {
     let arguments: Vec<String> = env::args().collect();
     let command = arguments[1].clone();
 
-    println!("{:#?}", arguments);
+    let todo_item = TodoItem{
+        name: "Say hi to rust!".to_string(),
+        completed: ' '
+    };
+
+    let todo_list = vec![todo_item];
+
+    if command == "get" {
+        for item in todo_list {
+            println!("[{}] - {}", item.completed, item.name);
+        }
+    }
 }
