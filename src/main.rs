4ac586e9d95f5731e790d09a8faec04a6e993b1e
Nico Audy <nicoaudy1994@gmail.com>
2020-07-21 09:11:12 +0700

    this is how to print args

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ use std::env;
 
 fn main() {
     let arguments: Vec<String> = env::args().collect();
-    println!("{}", arguments);
-    println!("Hello, world!");
+    println!("{:#?}", arguments);
 }
